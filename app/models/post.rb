2e5c2cb1a4381d06388565272619cff8faab4330
Shih Hsiang Wang <ultimateghost@gmail.com>
2016-01-01 17:31:16 +0800

    Chapter 9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :group, counter_cache: :posts_count
   validates :content, presence: true
+  scope :recent, -> { order("updated_at DESC")}
 
   belongs_to :author, class_name: "User", foreign_key: :user_id
 
